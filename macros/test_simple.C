7d90e5ac2516191b272c1adb75902da3e356bbd2
Nizar Septian <nizar47@gmail.com>
2025-07-24 15:36:29 -0400

    Ignore test macros

--- macros/test_simple.C
+++
@@ -1,8 +0,0 @@
-#include "MomentHelper.h"
-
-void test_simple() {
-    MomentHelper helper;
-    helper.Set("H_0_0_0", 2.0);
-    std::cout << "Value: " << helper.GetVal("H_0_0_0") << std::endl;
-    std::cout << "Test completed successfully!" << std::endl;
-}
